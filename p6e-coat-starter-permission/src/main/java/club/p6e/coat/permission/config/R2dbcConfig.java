109fddd2904f53cb10b150594ea2b019c7dc15be
lidashuang <1294935733@qq.com>
2023-10-13 18:29:03 +0800

    111

--- p6e-coat-starter-permission/src/main/java/club/p6e/coat/permission/config/R2dbcConfig.java
+++ p6e-coat-starter-permission/src/main/java/club/p6e/coat/permission/config/R2dbcConfig.java
@@ -4,6 +4,7 @@ import club.p6e.coat.permission.PermissionConfig;
 import io.r2dbc.postgresql.PostgresqlConnectionConfiguration;
 import io.r2dbc.postgresql.PostgresqlConnectionFactory;
 import io.r2dbc.spi.ConnectionFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
@@ -17,6 +18,10 @@ import org.springframework.stereotype.Component;
 public class R2dbcConfig {
 
     @Bean("club.p6e.coat.permission.config.ConnectionFactory")
+    @ConditionalOnMissingBean(
+            value = ConnectionFactory.class,
+            ignored = ConnectionFactory.class
+    )
     public ConnectionFactory injectConnectionFactory(PermissionConfig config) {
         return new PostgresqlConnectionFactory(
                 PostgresqlConnectionConfiguration
